ba5df4b2beac3edebc6c18a6b88f697457d20c19
Nebojsa Timotic <timotic.n89@gmail.com>
2025-07-29 23:43:51 +0200

    Auto-save: backup_before_cleanup\.git_backup\objects\4b\825dc642cb6eb9a060e54bf8d69288fbee4904 at 2025-07-29 23:43:51

--- backup_before_cleanup/db/migrations/setup_structure.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Struktura foldera
-folders = ["logs", "reports/daily", "reports/weekly", "reports/monthly", "reports/yearly"]
-
-for folder in folders:
-    os.makedirs(folder, exist_ok=True)
-
-print("Struktura foldera je uspešno napravljena.")
--- backup_before_cleanup/ml/__init__.py
+++
@@ -1,4 +0,0 @@
-# ml/__init__.py
-
-# Ovaj fajl omogućava da se 'ml' folder tretira kao Python modul.
-# Nije neophodno ništa da se piše u njemu, ali možeš da dodaš eventualne globalne inicijalizacije ako zatreba kasnije.

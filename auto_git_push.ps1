218b10da4202e260dbeba389d9c707f75d5d456b
Nebojsa Timotic <timotic.n89@gmail.com>
2025-07-30 15:36:11 +0200

    Auto-save: check_and_setup_vscode_extensions.py at 2025-07-30 15:36:10

--- auto_git_push.ps1
+++
@@ -1,8 +0,0 @@
-﻿# auto_git_push.ps1
-Set-Location $PSScriptRoot
-git add -A
-if (-not (git diff-index --quiet HEAD --)) {
-  $ts = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
-  git commit -m "Auto-save at $ts"
-  git push
-}

a21c42b55ece877e21678538786794e08b44e9d0
Nebojsa Timotic <timotic.n89@gmail.com>
2025-07-31 10:05:22 +0200

    Auto-commit from File Watcher at 2025-07-31 10:05:22

--- scripts/convert_run.py
+++
@@ -1,5 +0,0 @@
-from utils.data_converter import DataConverter
-
-if __name__ == "__main__":
-    converter = DataConverter()
-    converter.convert_all()  # poziva metodu iz klase koja konvertuje podatke
